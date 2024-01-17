c5db7004d944b7fc3c66433ee830f194e69b1210
codeabuu <abdulngui718@gmail.com>
2024-01-17 22:30:55 +0000

    updates

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,11 +1,7 @@
--- script to add marks
+-- Write a SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
 
 DELIMITER $$;
-CREATE PROCEDURE AddBonus(
-	IN in_user_id INT,
-	IN in_project_name VARCHAR(255),
-	IN score INT
-)
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT )
 BEGIN
 	IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
 		INSERT INTO projects (name) VALUES (project_name);
@@ -13,4 +9,6 @@ BEGIN
 	INSERT INTO corrections (user_id, project_id, score)
 	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
 END;$$
-DELIMITER;
+DELIMITER ;
+
+	
