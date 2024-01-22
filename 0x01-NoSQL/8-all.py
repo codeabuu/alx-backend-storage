dd406b97a2a67c2e377053e674825344114e5962
codeabuu <abdulngui718@gmail.com>
2024-01-22 09:41:51 -0800

    function that lists all documents in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Python function that lists all documents in a collection'''
+
+
+import pymongo
+
+
+def list_all(mongo_collection):
+    '''loop for return sttmt'''
+    if mongo_collection is None:
+        return ([])
+    lst = mongo_collection.find()
+    result = []
+    for i in lst:
+        result.append(i)
+    return result
---
+++ 0x01-NoSQL/8-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from pymongo import MongoClient
+list_all = __import__('8-all').list_all
+
+if __name__ == "__main__":
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    school_collection = client.my_db.school
+    schools = list_all(school_collection)
+    for school in schools:
+        print("[{}] {}".format(school.get('_id'), school.get('name')))
