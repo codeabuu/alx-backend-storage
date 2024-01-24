a43abd9167e5e233cddfe2b46b97017b6b78623f
Abdullahi Ngui <125456974+codeabuu@users.noreply.github.com>
2024-01-24 11:51:47 -0500

    redis commands and functions

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,47 +1,36 @@
 #!/usr/bin/env python3
-''' store an instance of the Redis client as a private variable named _redis'''
+"""0. Create a Cache class. In the __init__ method, store an instance of
+the Redis client as a private variable named _redis (using redis.Redis())
+and flush the instance using flushdb.
 
-from uuid import uuid4
+Create a store method that takes a data argument and returns a string.
+The method should generate a random key (e.g. using uuid), store the
+input data in Redis using the random key and return the key.
+
+Type-annotate store correctly. Remember that data can be a str,
+bytes, int or float.
+
+
+1. Redis only allows to store string, bytes and numbers (and lists thereof).
+Whatever you store as single elements, it will be returned as a byte string.
+Hence if you store "a" as a UTF-8 string, it will be returned as b"a" when
+retrieved from the server.
+
+In this exercise we will create a get method that take a key string argument
+and an optional Callable argument named fn. This callable will be used to
+convert the data back to the desired format.
+
+Remember to conserve the original Redis.get behavior if the key does not exist.
+Also, implement 2 new methods: get_str and get_int that will automatically
+parametrize Cache.get with the correct conversion function.
+"""
+
+
 import redis
+from uuid import uuid4
 from typing import Union, Callable, Optional
 from functools import wraps
 
-
-
-class Cache:
-    def __init__(self):
-        '''will store instance of redis'''
-        self._redis = redis.Redis()
-        self._redis.flushdb()
-
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        '''reutrn key'''
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-
-        return key
-
-    def get(self, key: str,
-            fn: Optional[callable] = None) -> Union[str, bytes, int, float]:
-        """convert the data back to the desired format"""
-        value = self._redis.get(key)
-        if fn:
-            value = fn(value)
-        return value
-
-    def get_str(self, key: str) -> str:
-        """automatically parametrize Cache.get with the correct"""
-        value = self._redis.get(key)
-        return value.decode("utf-8")
-
-    def get_int(self, key: str) -> int:
-        """automatically parametrize Cache"""
-        value = self._redis.get(key)
-        try:
-            value = int(value.decode("utf-8"))
-        except Exception:
-            value = 0
-        return value
 
 def count_calls(method: Callable) -> Callable:
     """returns a Callable"""
@@ -102,3 +91,43 @@ def replay(fn: Callable):
         # print(f"{function_name}(*{input}) -> {output}")
         print("{}(*{}) -> {}".format(function_name, input, output))
 
+
+class Cache:
+    """Create a Cache class"""
+
+    def __init__(self):
+        """store an instance of the Redis client"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @count_calls
+    @call_history
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """generate a random key"""
+        random_key = str(uuid4())
+        self._redis.set(random_key, data)
+        return random_key
+
+    def get(self, key: str,
+            fn: Optional[callable] = None) -> Union[str, bytes, int, float]:
+        """convert the data back to the desired format"""
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """automatically parametrize Cache.get with the correct
+        conversion function"""
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """automatically parametrize Cache.get with the correct
+        conversion function"""
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
